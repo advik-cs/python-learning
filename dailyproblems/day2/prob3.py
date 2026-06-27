6b7548fa6f5f27fd76808a1cd3ca167ce9a1fe50
advik-cs <advikd.hyd@gmail.com>
2026-06-27 15:11:13 +0530

    Second Commit

--- dailyproblems/day2/prob3.py
+++ dailyproblems/day2/prob3.py
@@ -1,21 +1,12 @@
 #Create a list of 10 random numbers between 10 and 50 
 #Then sum those 
 #then print that list and its sum 
-
 import random
 
-a = random.randint(10,50)
-b = random.randint(10,50)
-c = random.randint(10,50)
-d = random.randint(10,50)
-e = random.randint(10,50)
-f = random.randint(10,50)
-g = random.randint(10,50)
-h = random.randint(10,50)
-i = random.randint(10,50)
-j = random.randint(10,50)
-
-lst = [a,b,c,d,e,f,g,h,i,j]
-x = a+b+c+d+e+f+g+h+i+j
+lst = [1,1,1,1,1,1,1,1,1,1]
+for e in range(0,10,1):
+    lst[e] = random.randint(10,50)
+    
+x = lst[0] + lst[1] + lst[2] + lst[3] + lst[4] + lst[5] + lst[6] + lst[7] + lst[8] + lst[9]
 
 print(lst,", sum:",x)
